87ae65b8f90ec90e63f6db99ec8d2cfc10a91651
Oren Fix <orenfix@gmail.com>
2017-08-19 23:40:01 -0700

    Complete lesson on dynamically generating game boards (Day 10-11)

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,22 +1,46 @@
-const printBoard = board => {
-	console.log("Current Board:");
-	for (let i=0; i<board.length; i++) {
-		console.log(board[i].join(' | '));
+const generatePlayerBoard = (numberOfRows,numberOfColumns) => {
+	let board = [];
+	for ( let i = 0; i < numberOfRows; i++ ) {
+		let row = [];
+		for ( let j = 0; j < numberOfColumns; j++ ) {
+			row.push(' ');
+		}
+		board.push(row);
 	}
-//	console.log(board[0].join(' | '));
-//	console.log(board[1].join(' | '));
-//	console.log(board[2].join(' | '));
+	return board;
 };
 
-let board = [
-	[' ',' ',' '],
-	[' ',' ',' '],
-	[' ',' ',' ']
-];
 
-printBoard(board);
+const generateBombBoard = (numberOfRows,numberOfColumns,numberOfBombs) => {
+	let board = [];
+	for ( let i = 0; i < numberOfRows; i++ ) {
+		let row = [];
+		for ( let j = 0; j < numberOfColumns; j++ ) {
+			row.push(null);
+		}
+		board.push(row);
+	}
+	let numberOfBombsPlaced = 0;
+	while ( numberOfBombsPlaced < numberOfBombs ) {
+		let randomRowIndex = Math.floor(Math.random()*numberOfRows);
+		let randomColumnIndex = Math.floor(Math.random()*numberOfColumns);
+		board[randomRowIndex][randomColumnIndex] = 'B';
+		numberOfBombsPlaced++;
+		/*
+		The code in your while loop has the potential to place bombs on top
+		of already existing bombs. This will be fixed when you learn about 
+		control flow.
+		*/
+	}
+	return board;
+};
 
-board[0][1] = '1';
-board[2][2] = 'B';
+const printBoard = board => console.log(board.map(row => row.join(' | ')).join('\n'));
 
-printBoard(board);
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player Board:');
+printBoard(playerBoard);
+console.log('Bomb Board:');
+printBoard(bombBoard);
